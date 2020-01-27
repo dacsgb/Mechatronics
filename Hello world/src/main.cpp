1f63a6ae537694c6df02262e19a3e4ea3580aa0e
dacsgb <43976345+dacsgb@users.noreply.github.com>
2020-01-27 11:45:36 -0600

    HW 1

---
+++ Hello world/src/main.cpp
@@ -0,0 +1,24 @@
+#include <Arduino.h>
+
+int i = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  do{
+  Serial.println("Diego Colón");
+  i++;
+  delay(1000);
+  }
+  while(i<5);
+  delay(1000);
+  Serial.print("Printed ");
+  Serial.print(i);
+  Serial.println(" Times");
+  delay(1000);
+  i = 0;
+}
